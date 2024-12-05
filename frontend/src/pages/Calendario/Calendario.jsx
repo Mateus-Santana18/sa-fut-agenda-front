1f981580c1e93f9aa7f91e76638e936881d22374
Mateus-Santana18 <mateus.teteu897@gmail.com>
2024-12-05 18:24:39 -0300

    editar usuario

--- frontend/src/pages/Calendario/Calendario.jsx
+++ frontend/src/pages/Calendario/Calendario.jsx
@@ -7,10 +7,10 @@ import { useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import 'react-clock/dist/Clock.css';
+import { FiArrowLeft } from 'react-icons/fi';
 import Modal from 'react-modal';
 import { useNavigate } from 'react-router';
 import LogoCalendario from '../../components/LogoCalendario/LogoCalendario';
-import { FiArrowLeft } from 'react-icons/fi';
 
 const customStyles = {
     content: {
@@ -30,47 +30,58 @@ const Calendario = () => {
     const navigate = useNavigate();
     const [isOpen, setIsOpen] = useState(false);
     return (
-        <div className="h-screen bg-black flex">
-            <div className="bg-[#00FF00] mt-2 ml-2 w-10 rounded-[50%] flex justify-center items-center h-10 cursor-pointer" onClick={() => navigate(-1)}>
-                <FiArrowLeft className="icon" />
+        <div className="h-screen bg-black flex flex-col">
+            <div className='flex w-full justify-between'>
+                <div className="bg-[#00FF00] mt-2 ml-2 w-10 rounded-[50%] flex justify-center items-center h-10 cursor-pointer" onClick={() => navigate('/login', {
+                    replace: true
+                })}>
+                    <FiArrowLeft className="icon" />
+                </div>
+                <div className="bg-[#00FF00] mt-2 ml-2 w-10 rounded-[50%] flex justify-center items-center h-10 cursor-pointer" onClick={() => {
+                    navigate('/profile-edit')
+                }}>
+                    <img className='icon' src="../../images/usuario-de-perfil.png" />
+                </div>
             </div>
-            <div className='ml-8 flex justify-center items-center'>
-                <LogoCalendario />
-            </div>
-            <div className='flex flex-row flex-1 justify-center items-center w-full h-full'>
-                <div className='flex flex-row items-center gap-8 border border-green-400 pr-4'>
-                    <Calendar onChange={date => {
-                        if (date.getDay() >= new Date().getDay()) {
-                            setIsOpen(true);
-                            setDate(date)                    
-                        }
-                    }} value={date} className="w-full h-full" />
-                    <Modal
-                        style={customStyles}
-                        isOpen={isOpen}
-                        contentLabel="Example Modal"
-                    >
-                        <div className='flex flex-1 justify-end'>
-                            <CloseOutlined className='cursor-pointer' onClick={() => setIsOpen(false)}/>
-                        </div>
-                        <div className='flex flex-col gap-1'>
-                            <span>
-                                Selecione seu horario: 
-                            </span>
-                            <Select 
-                                className='w-72 text-white'
-                                onChange={(e) => {
-                                    navigate('/hour-list/' + `${format(date, 'yyyy-MM-dd')}:${e}`);
-                                }}
-                                options={hours.map(h => {
-                                    return {
-                                        label: `${h}:00`,
-                                        value: h
-                                    }
-                                })}
-                            />
-                        </div>
-                    </Modal>
+            <div className='flex h-screen'>
+                <div className='ml-8 flex justify-center items-center'>
+                    <LogoCalendario />
+                </div>
+                <div className='flex flex-row flex-1 justify-center items-center w-full h-full'>
+                    <div className='flex flex-row items-center gap-8 border border-green-400 pr-4'>
+                        <Calendar onChange={date => {
+                            if (date.getDay() >= new Date().getDay()) {
+                                setIsOpen(true);
+                                setDate(date)                    
+                            }
+                        }} value={date} className="w-full h-full" />
+                        <Modal
+                            style={customStyles}
+                            isOpen={isOpen}
+                            contentLabel="Example Modal"
+                        >
+                            <div className='flex flex-1 justify-end'>
+                                <CloseOutlined className='cursor-pointer' onClick={() => setIsOpen(false)}/>
+                            </div>
+                            <div className='flex flex-col gap-1'>
+                                <span>
+                                    Selecione seu horario: 
+                                </span>
+                                <Select 
+                                    className='w-72 text-white'
+                                    onChange={(e) => {
+                                        navigate('/hour-list/' + `${format(date, 'yyyy-MM-dd')}:${e}`);
+                                    }}
+                                    options={hours.map(h => {
+                                        return {
+                                            label: `${h}:00`,
+                                            value: h
+                                        }
+                                    })}
+                                />
+                            </div>
+                        </Modal>
+                    </div>
                 </div>
             </div>
         </div>
--- frontend/src/pages/Perfil/Perfil.jsx
+++ frontend/src/pages/Perfil/Perfil.jsx
@@ -1,19 +1,33 @@
-import React from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router';
+import { toast, ToastContainer } from 'react-toastify';
+import { useAuth } from '../../components/Authentication/useAuth';
+import api from '../../config/axios';
 import './Perfil.css';
-import LogoPerfil from '../../components/LogoPerfil/LogoPerfil';
 
 const Perfil = () => {
-
-
-
-
+    const navigate = useNavigate();
+    const {
+        user,
+        saveToken
+    } = useAuth();
+    const [nome, setNome] = useState(user.nome);
+    const [email, setEmail] = useState(user.email);
+    const [telefone, setTelefone] = useState(user.telefone);
+    const [password, setPassword] = useState();
+    // useEffect(() => {
+    //     setNome(user.nome)
+    //     setEmail(user.email)
+    //     setTelefone(user.telefone)
+    // }, [user]);
     return (
         <div className='PerfilContainer'>
+            <ToastContainer />
             <div className='topoPerfil'>
 
             </div>
             <div className='meioPerfil'>
-                <div className='LadoEsquerdoPerfil'>
+                {/* <div className='LadoEsquerdoPerfil'>
                     <div className='NomeUsuarioPerfil'>
                         <h1 className='nome'>#fulano</h1>
                     </div>
@@ -24,44 +38,66 @@ const Perfil = () => {
                         <LogoPerfil />
                     </div>
                     <div className='DataPerfil'>
-                        <h1 className='nome'>dia talksksksk</h1>
+                        <h1 className='nome text-white'>dia talksksksk</h1>
                     </div>
-                </div>
+                </div> */}
                 <div className='LadoDireitoPerfil'>
                     <div className='cimaMeioPerfil'>
                         <div className='tituloPerfil'>
-                            <h1 className='EditarPerfil'>Editar Perfil</h1>
+                            <h1 className='EditarPerfil text-white'>Editar Perfil</h1>
                         </div>
                         <div className='infoUsuario'>
-                            <h1 className='EditarPerfil2'>Informações de usuário</h1>
+                            <h1 className='EditarPerfil2 text-white'>Informações de usuário</h1>
                         </div>
                     </div>
                     <div className='dadosPerfilCima'>
                         <div className='inptS'>
-                            <label className='nomes'>Nome Completo</label>
-                            <input className='inpt' />
+                            <label className='nomes text-white'>Nome Completo</label>
+                            <input className='inpt' value={nome} onChange={e => setNome(e.target.value)} />
                         </div>
                         <div className='inptS'>
-                            <label className='nomes'>Senha</label>
+                            <label className='nomes text-white'>Senha</label>
                             <input className='inpt'
-                                type='password' />
+                                type='password' value={password} onChange={e => setPassword(e.target.value)} />
                         </div>
                     </div>
                     <div className='dadosPerfilBaixo'>
                         <div className='inptS'>
-                            <label className='nomes'>E-mail</label>
-                            <input className='inpt' />
+                            <label className='nomes text-white'>E-mail</label>
+                            <input className='inpt' value={email} onChange={e => setEmail(e.target.value)} />
                         </div>
                         <div className='inptS'>
-                            <label className='nomes'>Telefone</label>
-                            <input className='inpt' />
+                            <label className='nomes text-white'>Telefone</label>
+                            <input className='inpt' value={telefone} onChange={e => setTelefone(e.target.value)} />
                         </div>
                     </div>
                     <div className='DivBotaoPerfil'>
-
-                        <div className='botaoPerfil'>
-                            <button className="botaoSalvarPerfil">
+                        <div className='flex gap-12'>
+                            <button className="botaoSalvarPerfil" onClick={() => {
+                                api.put('/auth', {
+                                    email,
+                                    nome,
+                                    senha: password,
+                                    telefone,
+                                    cargo: 'USER'
+                                }).then((r) => {
+                                    saveToken(r.data)
+                                }).catch(() => {
+                                    toast('Falha ao atualizar os dados')
+                                })
+                            }}>
                                 SALVAR
+                            </button>
+                            <button className="botaoDeletarPerfil" onClick={() => {
+                                api.delete('/auth')
+                                .then(() => {
+                                    navigate('/login')
+                                })
+                                .catch(() => {
+                                    toast('Falha ao deletar a conta')
+                                })
+                            }}>
+                                DELETAR CONTA
                             </button>
                         </div>
                     </div>
